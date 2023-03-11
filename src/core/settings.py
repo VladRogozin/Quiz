873918496605de3182a611ffddaf5ffaf89bf8a3
Vlad Rogozin <vroghozin@gmail.com>
2023-03-11 17:44:15 +0200

    Work 24

--- src/core/settings.py
+++ src/core/settings.py
@@ -15,18 +15,13 @@ from pathlib import Path
 from django.urls import reverse_lazy
 from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '../.env')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DJANGO_DEBUG').lower() in ('true', 'on', '1')
 
 ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS').split()
